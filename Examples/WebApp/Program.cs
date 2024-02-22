f87cab12b50b0524c05678e512e67931637912fc
Hassan Aliakbari <Amir.Aliakbari@hotmail.com>
2024-02-22 02:24:53 -0500

    -

--- Examples/WebApp/Program.cs
+++ Examples/WebApp/Program.cs
@@ -1,3 +1,5 @@
+using WebApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddHostedService<GeoLayers>();
+builder.Services.AddSingleton<GeoLayers>();
 
 var app = builder.Build();
 
